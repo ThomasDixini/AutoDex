bb798904e366ac5ca233383ae091c43186bbbbfe
ThomasDixini <thomasdixini.dev@gmail.com>
2026-02-14 15:09:14 -0300

    :tada: Initial Commit

---
+++ src/main/java/com/thomasdixini/autodex/AutodexApplication.java
@@ -0,0 +1,13 @@
+package com.thomasdixini.autodex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutodexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutodexApplication.class, args);
+	}
+
+}
